834c6bca3a3a079b0fd60a28508af8b77f836cb5
20160000290-MartinSoria <m.soria.5ddb@gmail.com>
2026-04-16 12:12:42 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,48 +2,31 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Escriba dos volores numericos");
-        double num1 = Convert.ToDouble(Console.ReadLine());
-        double num2 = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("\n***MENU PRENCIPAL**");
-        Console.WriteLine("1. Sumar");
-        Console.WriteLine("2. Resta");
-        Console.WriteLine("3. Multiplicacion");
-        Console.WriteLine("4. Divicion");
-        Console.WriteLine("5. Salir");
-        Console.WriteLine("Dijite el Valor segun la operacion: []");
-        int opc = Convert.ToInt32(Console.ReadLine());
-
-        //Evaluar con swith
-        string mjs = "";
-        double result = 0;
-        switch (opc)
+        Console.WriteLine("RENGO DE ESTUDIENTES");
+        Console.WriteLine("Ingrese una letre de estas: A, B, C, D Y F");
+        char letra = Char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+        switch (letra)
         {
-            case 1:
-                mjs = "\nSUMA \nLa suma es: " + (num1 + num2);
+            case 'A':
+                Console.WriteLine("EXELENTE");
                 break;
-            case 2:
-                mjs = "\nRESTA \nLa resta es: " + (num1 - num2);
+            case 'B':
+                Console.WriteLine("BUENO");
                 break;
-            case 3:
-                mjs = "\nMULTIPLICACION \nLa multiplicacion es: " + (num1 * num2);
+            case 'C':
+                Console.WriteLine("REGULAR");
                 break;
-            case 4:
-                mjs = "\nDIVICION \nLa divicion es: " + (num1 / num2);
+            case 'D':
+                Console.WriteLine("DEFICIENTE");
                 break;
-            case 5:
-                mjs = "Saliendo del sistema!!!";
+            case 'F':
+                Console.WriteLine("REPROBADO");
                 break;
             default:
-                mjs = "Dijite  una Opcion valida";
+                Console.WriteLine("Escriva una letra valida");
                 break;
 
         }
-        Console.Clear();
-        Console.BackgroundColor = ConsoleColor.Yellow;
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine(mjs);
-        Console.WriteLine(result);
     }
 }
---
+++ ejercici4.psc
@@ -0,0 +1,37 @@
+Algoritmo ejercici4
+	Definir opc Como Caracter
+
+	
+	Escribir "***********************"
+	Escribir "MENÚ PRINCIPAL"
+	Escribir "1-HMBURGUESA"
+	Escribir "2-PIZZA"
+	Escribir "3-POLLO FRITO"
+	Escribir "4-ENSALADA"
+	Escribir "5-Salir"
+	Escribir Sin Saltar "Dijite un menu: "
+	Leer opc
+	
+	Segun opc Hacer
+		1:
+			Escribir  "Usted ha solisitado -->"
+			Escribir "Un platillo de Hamburguesa"
+		2:
+			Escribir  "Usted ha solisitado -->"
+			Escribir "Un platillo de Pizza"
+		3:
+			Escribir  "Usted ha solisitado -->"
+			Escribir "Un platillo de Pollo Frito"
+		4:
+			Escribir  "Usted ha solisitado -->"
+			Escribir "Un platillo de Ensalada"
+		5:
+			Escribir  "Usted ha solisitado -->"
+			Escribir "Un platillo de Hamburguesa"
+		De Otro Modo:
+			Escribir "Numero no valido"
+	FinSegun
+	
+
+FinAlgoritmo
+
